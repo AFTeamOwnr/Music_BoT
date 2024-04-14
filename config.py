70f9d5aed46481bc299a217dedcc5dcfce87628a
AFTeamOwnr <167010119+AFTeamOwnr@users.noreply.github.com>
2024-04-15 01:11:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ API_HASH = getenv("d7faf341162a27f8d835d2d0b32bf756")
 BOT_TOKEN = getenv("7045730156:AAFeRdvjT7M5Sba-sgwpJEmudzAZqXbmknc")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("mongodb+srv://musicbot1122:musicbot1122@cluster0.xa7d4aj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("-4198575476", None))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("5065276380"))
