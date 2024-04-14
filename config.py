566a354e07ff85e91e565095f5fedaa9d61b5c32
AFTeamOwnr <167010119+AFTeamOwnr@users.noreply.github.com>
2024-04-15 01:30:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ OWNER_ID = int(getenv("5065276380"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_APP_NAME = getenv("afteammusicbot")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
